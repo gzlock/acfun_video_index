afe036f3848e166a6a67173aeb99af21b85da28e
gzlock <gzlock88@gmail.com>
2021-11-03 02:55:59 +0800

    20211103

--- src/queryContributeList.ts
+++ src/queryContributeList.ts
@@ -24,12 +24,12 @@ export function queryContributeList (
     'https://member.acfun.cn/list/api/queryContributeList',
     `pcursor=${page}&resourceType=2&sortType=3&authorId=${authorId}&status=${status}`,
     {
+      timeout: 10000, // 10秒超时
       headers: {
         cookie: process.env.ACFUN_COOKIES,
       },
-    }).
-    then(res => ({
-      page: res.data.pcursor,
-      list: res.data.feed.map(data => new Feed(data)),
-    }))
+    }).then(res => ({
+    page: res.data.pcursor,
+    list: res.data.feed.map(data => new Feed(data)),
+  }))
 }
