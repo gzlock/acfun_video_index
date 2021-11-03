b15b526ed5befc2244d566c5cca258f656b6cbd6
gzlock <gzlock88@gmail.com>
2021-11-03 18:26:19 +0800

    20211103

--- src/personalBasicInfo.ts
+++ src/personalBasicInfo.ts
@@ -7,7 +7,13 @@ import axios, {AxiosResponse} from "axios";
 export function personalBasicInfo<T>(): Promise<AxiosResponse<T>> {
     return axios.get<T>('https://www.acfun.cn/rest/pc-direct/user/personalBasicInfo', {
         headers: {
-            cookie: process.env.ACFUN_COOKIES as string
+            cookie: process.env.ACFUN_COOKIES as string,
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "zh-CN,zh;q=0.9",
+            "cache-control": "no-cache",
+            "content-type": "application/x-www-form-urlencoded",
+            "pragma": "no-cache",
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
         }
     })
 }
--- src/queryContributeList.ts
+++ src/queryContributeList.ts
@@ -27,6 +27,12 @@ export function queryContributeList (
       timeout: 10000, // 10秒超时
       headers: {
         cookie: process.env.ACFUN_COOKIES as string,
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/x-www-form-urlencoded",
+        "pragma": "no-cache",
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
       },
     }).then(res => ({
     page: res.data.pcursor,
